b05c539ef21c2d8bb3f36dacd03f6dcf04ea62de
alex.sharov <alex.sharov@lazada.com>
2016-12-06 16:55:31 +0700

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+composer require phpstan/phpstan &
+wget -c https://static.phpmd.org/php/latest/phpmd.phar -O phpmd &
+wget -c https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar -O phpcs &
+wget -c https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar -O phpcbf &
+wget -c https://github.com/Trismegiste/Mondrian/raw/master/bin/box/mondrian.phar -O mondrian &
+wget -c https://github.com/ovr/phpsa/releases/download/0.6.1/phpsa.phar -O phpsa &
+wget -c https://github.com/etsy/phan/releases/download/0.6/phan.phar -O phan &
+
+wait
---
+++ run.sh
@@ -0,0 +1,2 @@
+
+docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php bash
